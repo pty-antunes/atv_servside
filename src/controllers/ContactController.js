2cfd92de4386f5734efe63f63e5d1bc95eef9c50
patricia <110406777+pty-antunes@users.noreply.github.com>
2024-11-13 15:53:54 -0300

    Update ContactController.js

--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -1,10 +1,12 @@
 const ContactRepository = require('../repositories/ContactRepository')
 
 class ContactController{
+
     async index(request, response){
         const contacts = await ContactRepository.findAll()
         response.json(contacts)
     }
+
     async show(request, response){
         const {id} = request.params
         const contact = await ContactRepository.findById(id)
@@ -13,15 +15,38 @@ class ContactController{
         }
         response.status(200).json(contact)
     }
-    store(){
 
+    async store(request, response){
+        const {name, email, phone, category_id} = request.body
+        if(!name){
+            return response.status(400).json({error: "Name is required"})
+        }
+        if(email){
+            const contactByEmail = await ContactRepository.findByEmail(email);
+            if(contactByEmail){
+                return response.status(400).json({error: "This e-mails is already in use"})
+            }
+        }
+        const contact = await ContactRepository.create({
+            name,
+            email: email || null,
+            phone: phone || null,
+            category_id: category_id || null
+        });
+        response.status(201).json(contact)  
     }
+
     update(){
 
     }
-    delete(){
-
-    }
+    
+    async delete(request, response){
+        const {id} = request.params
+        if(!id)
+            return response.status(400).json({error: 'Invalid'})
+    await ContactRepository.delete(id);
+    response.sendStatus(204);
+}
 }
 
 module.exports = new ContactController();
