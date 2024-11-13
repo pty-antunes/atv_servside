94717a70ad67dccaceaaaedea66a27b0bc0a6501
patricia <110406777+pty-antunes@users.noreply.github.com>
2024-11-13 15:54:36 -0300

    Update ContactRepository.js

--- src/repositories/ContactRepository.js
+++ src/repositories/ContactRepository.js
@@ -1,12 +1,14 @@
 const db = require('../models/ConnectDatabase')
 
 class ContactRepository {
+
     async findAll(){
         const rows = await db.query(
             `select * from contacts;`
         )
         return rows
     }
+
     async findById(id){
         const [row] = await db.query(
             `select contacts. *, categories.name as category_name
@@ -18,14 +20,45 @@ class ContactRepository {
         )
         return row
     }
-    create(){
 
+    async findByEmail(email){
+        const [rows] = await db.query(
+            `SELECT * FROM contacts
+            WHERE email = ?;
+            `,
+            [email]
+        )
+        return rows
     }
+
+    async create(name, email, phone, category_id){
+        const result = await db.query(
+            `INSERT INTO contacts(name, email, phone, category_id)
+            VALUES (?, ?, ?, ?)
+            `,
+            [name, email, phone, category_id]
+        )
+        const insertId = result.insertId
+        return{
+            id: insertId,
+            name,
+            email,
+            phone,
+            category_id
+        }
+    }
+
     update(){
 
     }
-    delete(){
 
+    async delete(id){
+        const deleteItem = await db.query(
+            `DELETE FROM contacts
+            WHERE id = ?`,
+            [id]
+        )
+        return deleteItem
     }
 }
 
